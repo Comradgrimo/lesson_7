8a5481c634d4e1623b8f6bfed67e60865adeed9d
Grimo <grimoreshmeda34641991>
2020-06-02 01:56:05 +0300

    add task_2

--- task_2.py
+++ task_2.py
@@ -1,6 +1,38 @@
-class Dress:
-    def __init__(self):
-        pass
+# 2. Реализовать проект расчета суммарного расхода ткани на производство одежды. Основная сущность (класс) этого проекта
+# — одежда, которая может иметь определенное название. К типам одежды в этом проекте относятся пальто и костюм. У этих
+# типов одежды существуют параметры: размер (для пальто) и рост (для костюма). Это могут быть обычные числа: V и H,
+# соответственно.
+# Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (V/6.5 + 0.5), для костюма
+# (2 * H + 0.3). Проверить работу этих методов на реальных данных.
+# Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания: реализовать
+# абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property.
 
-    def palto(selfs):
-        pass
+
+class Dress(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def get_square_c(self):
+        return self.width / 6.5 + 0.5
+
+    def get_square_j(self):
+        return 2 * self.height + 0.3
+
+    @property
+    def get_sq_full(self):
+        return str(f'Площадь общая ткани {self.get_square_c() + self.get_square_j()}')
+
+class Coat(Dress):
+    def __str__(self):
+        return f'Площадь на пальто {self.get_square_c()}'
+
+class Jacket(Dress):
+    def __str__(self):
+        return f'Площадь на костюм {self.get_square_j()}'
+
+d= Dress(10,20)
+
+print(d.get_sq_full)
+print(Jacket(10, 20))
+print(Coat(10, 20))
